be05c8c58a0c2f848fede6a32149444972d2f97d
Nishu Tyagi <tyaginishu7@gmail.com>
2021-08-06 13:23:19 +0530

    Refactored:To create Generic Class to take 3 Variables of Generic Type

--- TestMaximum.java
+++ TestMaximum.java
@@ -28,6 +28,24 @@ public class TestMaximum <N extends Comparable<N>> {
 		return max; // returns the largest object
 	}
 
+	public static String testMaximum(String X, String Y, String Z) {
+	
+		String max = X; 
+		if(Y.compareTo(max) > 0) {
+		
+			max = Y; //y is the largest so far
+		
+		} 
+		if(Z.compareTo(max) > 0) {
+		
+			max = Z; // z is the largest now
+		
+		}
+	
+		printMax(X, Y, Z, max); 
+		return max; // returns the largest object
+	}
+
 	public static <N extends Comparable> void printMax(N X, N Y, N Z, N max) {
 		System.out.printf("Maximum Integer From %s, %s and %s is %s\n",X, Y, Z, max);
 	}
@@ -38,6 +56,7 @@ public class TestMaximum <N extends Comparable<N>> {
 		Float  XFloat = 6.9f , YFloat = 7.2f, ZFloat = 9.1f;
 		String XStr = "Parrot", YStr = "Rabbit", ZStr = "Lion"; 
 		
+	    TestMaximum.testMaximum(XStr, YStr, ZStr);
 		TestMaximum.maximum(XInt, YInt, ZInt);
 		TestMaximum.maximum(XFloat, YFloat, ZFloat);
 		TestMaximum.maximum(XStr, YStr, ZStr);
