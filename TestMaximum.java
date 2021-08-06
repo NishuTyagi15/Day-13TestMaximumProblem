353fb58d2d9eda2ea5106578a4d23eecc09f6d69
Nishu Tyagi <tyaginishu7@gmail.com>
2021-08-06 11:21:50 +0530

    Added:Find the Maximum of 3 Integers Program

--- TestMaximum.java
+++ TestMaximum.java
@@ -1,5 +1,58 @@
 public class TestMaximum {
-    public static void main (String[] args) {
-        System.out.println("Welcome to the Test Maximum of 3 Variables using Generics");
-    }
+    Integer X,Y,Z;
+
+
+	
+
+	public TestMaximum(Integer X, Integer Y, Integer Z) {
+		
+		this.X = X;
+		this.Y = Y;
+		this.Z = Z;
+	}
+
+
+	public int maximum() {
+	
+		return TestMaximum.maximum(X, Y, Z); 
+	}
+
+	
+	// determines the largest integer from 3 Integers
+	public static int maximum(Integer X, Integer Y, Integer Z) { 
+		Integer max = X; // assume initially X is the largest integer
+	
+
+		if(Y.compareTo(max) > 0) { 
+			max = Y; //y is the largest integer so far
+		}
+	
+		if(Z.compareTo(max) > 0) {
+		
+			max = Z; // z is the largest integer now
+		}
+		
+		printMax(X, Y, Z, max);
+		
+		return max; // returns the largest object
+	}
+
+	
+	public static  void printMax(Integer X, Integer Y, Integer Z, Integer max) {
+		System.out.printf("Maximum Integer From %s, %s and %s is %s\n",X, Y, Z, max);
+	}
+
+	public static void main(String args[]) {
+
+		Integer XInt=10, YInt = 5, ZInt = 6;
+	
+		
+		// MaximumTest.testMaximum(xInt, yInt , zInt); 
+		
+		new TestMaximum(XInt, YInt, ZInt).maximum(); // for TC 1.1
+		new TestMaximum(YInt, XInt, ZInt).maximum(); // for TC 1.2
+		new TestMaximum(ZInt, YInt, XInt).maximum(); // for TC 1.3
+		
+		
+	}
 }
